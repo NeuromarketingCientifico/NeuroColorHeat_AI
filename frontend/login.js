69fb0d3a5d1c4707d4692aa3b6a1c9c5900180e7
NeuromarketingCientifico <neuromarketingcientifico@gmail.com>
2025-03-24 03:36:20 -0600

    Se agrego la validación del nombre.

--- frontend/login.js
+++ frontend/login.js
@@ -4,11 +4,12 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Validar que la contraseña sea la correcta
-    if (password === "admin123") {
-        // Si la contraseña es correcta, redirigir al index
+    // Validar que el usuario sea 'admin' y la contraseña sea 'admin123'
+    if (username === "admin" && password === "admin123") {
+        // Si el usuario y la contraseña son correctos, redirigir al index
+        sessionStorage.setItem("loggedIn", "true");  // Guardamos el estado de login
         window.location.href = "index.html";  // Redirige a la página principal
     } else {
-        alert("Contraseña incorrecta. Inténtalo de nuevo.");
+        alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
     }
 });
